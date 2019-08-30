c2c26e2ef7c0a898e5704ee40ceb21bbd49715f6
graffiiee <52782558+graffiiee@users.noreply.github.com>
2019-08-30 14:33:17 +0800

    Add files via upload

---
+++ arkanoid0/src/constants.lua
@@ -0,0 +1,16 @@
+--[[
+     CMPE40032 2018
+    Arkanoid Remake
+
+    -- constants --
+
+    Some global constants for our application.
+]]
+
+-- size of our actual window
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- size we're trying to emulate with push
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
